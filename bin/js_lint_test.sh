4f8f50380b702f12563da0bbaf08003434fbae4e
Paul Sealock <psealock@gmail.com>
2020-07-30 11:33:38 +1200

    test

--- bin/js_lint_test.sh
+++ bin/js_lint_test.sh
@@ -3,9 +3,14 @@
 
 set -o errexit
 
+echo "starting"
+
 if [[ ${RUN_JS} == 1 ]]; then
+echo "install"
 	npm run -s install-if-deps-outdated
+echo "linting"
 	npm run lint:js client/**/*.js
+echo "building"
 	npm run build
 	npm run test:unit
 fi

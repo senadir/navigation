234159b6db4fb96c9b38b77f9d59bce34f5b9b99
Paul Sealock <psealock@gmail.com>
2020-07-30 11:41:29 +1200

    all js files

--- bin/js_lint_test.sh
+++ bin/js_lint_test.sh
@@ -9,7 +9,7 @@ if [[ ${RUN_JS} == 1 ]]; then
 echo "install"
 	npm run -s install-if-deps-outdated
 echo "linting"
-	npm run lint:js client/**/*.js
+	npm run lint:js client/**/*.js client/*.js
 echo "building"
 	npm run build
 	npm run test:unit

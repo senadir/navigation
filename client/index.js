ca5085765358fb836e5196da4fadd5984fa194d5
Paul Sealock <psealock@gmail.com>
2020-07-30 11:50:09 +1200

    rename again

--- client/index.js
+++ client/index.js
@@ -7,7 +7,7 @@ import { render } from '@wordpress/element';
  * Internal dependencies
  */
 import './stylesheets/index.scss';
-import Navigation from './_navigation';
+import Navigation from './navigation';
 
 const navigationRoot = document.getElementById(
 	'woocommerce-embedded-navigation'

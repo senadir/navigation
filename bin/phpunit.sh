128ed53545a68c909c6a2b6a4698ce53a7f4575e
Paul Sealock <psealock@gmail.com>
2020-07-30 12:12:25 +1200

    come on travis

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 WORKING_DIR="$PWD"
 if [[ {$COMPOSER_DEV} == 1 ]]; then
-	echo "composer dev"
 	./vendor/bin/phpunit --version
 	if [[ {$RUN_RANDOM} == 1 ]]; then
 		./vendor/bin/phpunit -c phpunit.xml.dist --order-by=random

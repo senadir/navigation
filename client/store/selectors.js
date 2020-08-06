f90c1ad74e3a7e3205daa29312a3ac6c60b8021e
Joshua T Flowers <joshuatf@gmail.com>
2020-08-06 15:38:00 +0300

    Add menu items filter to frontend (#58)
    * Add menu items filter to frontend
    
    * Add external dependencies block comment

--- client/store/selectors.js
+++ client/store/selectors.js
@@ -1,9 +1,20 @@
+/**
+ * External dependencies
+ */
+import { applyFilters } from '@wordpress/hooks';
+
+const MENU_ITEMS_HOOK = 'woocommerce_navigation_menu_items';
+
 export const getMenuItems = ( state, id = null ) => {
 	if ( ! id ) {
-		return state.menuItems;
+		return applyFilters( MENU_ITEMS_HOOK, state.menuItems );
 	}
 
-	return state.menuItems.filter( ( item ) => item.menuId === id );
+	return applyFilters(
+		MENU_ITEMS_HOOK,
+		state.menuItems.filter( ( item ) => item.menuId === id ),
+		id
+	);
 };
 
 export const getActiveItem = ( state ) => {
